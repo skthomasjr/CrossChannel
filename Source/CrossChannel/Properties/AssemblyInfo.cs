7f2744b3aadb80643227b0d175772c702bafd89f
Scott Keith Thomas, Jr <skthomasjr@users.noreply.github.com>
2016-06-09 21:17:42 -0600

    Add files via upload

---
+++ Source/CrossChannel/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CrossChannel")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CrossChannel")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("4f319a7f-ba3e-45ea-b064-2e2ab58cf0e1")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
